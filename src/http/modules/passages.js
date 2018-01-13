cce61490bde0d892c9ed75677a71fad4e20cde4e
Rafael Ribeiro <rafael.pr10@gmail.com>
2018-01-13 11:56:12 -0200

    Incluído rotas para obter as próximas passagens e passagens anteriores

--- src/http/modules/passages.js
+++ src/http/modules/passages.js
@@ -2,9 +2,27 @@
 const db = require('../../services/mysql')
 
 module.exports = function passages (server) {
-  server.get('passages/:id', async (req, res, next) => {
+  server.get('/passages/:id', async (req, res, next) => {
     try {
       res.send(await db.passages().one(req.params.id))
+    } catch (error) {
+      res.send(error)
+    }
+    next()
+  })
+
+  server.get('/passages/:id/next', async (req, res, next) => {
+    try {
+      res.send(await db.passages().next(req.params.id))
+    } catch (error) {
+      res.send(error)
+    }
+    next()
+  })
+
+  server.get('/passages/:id/previous', async (req, res, next) => {
+    try {
+      res.send(await db.passages().previous(req.params.id))
     } catch (error) {
       res.send(error)
     }
--- src/services/mysql/tests/passages.test.js
+++ src/services/mysql/tests/passages.test.js
@@ -8,3 +8,13 @@ test('Obter Passagem por Id', async t => {
   const expected = await passages.one(1)
   t.is(expected.passage.text, 'No princípio Deus criou os céus e a terra.')
 })
+
+test('Obter Próxima Passagem Bíblica Válida', async t => {
+  const expected = await passages.next(31061)
+  t.is(expected.passage.text, 'A graça do Senhor Jesus seja com todos. Amém.')
+})
+
+test('Obter Passagem Bíblica Anterior Válida', async t => {
+  const expected = await passages.previous(2)
+  t.is(expected.passage.text, 'No princípio Deus criou os céus e a terra.')
+})
