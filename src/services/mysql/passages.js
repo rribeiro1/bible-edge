2d3140896cc72705ada1295de64f26feef4ca667
Rafael Ribeiro <rafael.pr10@gmail.com>
2018-01-20 17:24:51 -0200

    Ajustes payload Passagem

--- src/services/mysql/passages.js
+++ src/services/mysql/passages.js
@@ -5,7 +5,7 @@ const passages = deps => {
       return new Promise((resolve, reject) => {
         const { connection, errorHandler } = deps
 
-        const query = 'select v.id, b.name as book, v.chapter, v.verse, v.text from verses as v inner join books as b on v.book = b.id where v.id = ?'
+        const query = 'select v.id, b.id as bookId, b.name as book, v.chapter, v.verse, v.text from verses as v inner join books as b on v.book = b.id where v.id = ?'
 
         connection.query(query, id, (error, results) => {
           if (error) {
@@ -20,7 +20,7 @@ const passages = deps => {
       return new Promise((resolve, reject) => {
         const { connection, errorHandler } = deps
 
-        const query = 'select v.id, b.name as book, v.chapter, v.verse, v.text from verses as v inner join books as b on v.book = b.id where v.id > ? order by v.id limit 1'
+        const query = 'select v.id, b.id as bookId, b.name as book, v.chapter, v.verse, v.text from verses as v inner join books as b on v.book = b.id where v.id > ? order by v.id limit 1'
 
         connection.query(query, id, (error, results) => {
           if (error || results[0] === undefined) {
@@ -35,12 +35,11 @@ const passages = deps => {
       return new Promise((resolve, reject) => {
         const { connection, errorHandler } = deps
 
-        const query = 'select v.id, b.name as book, v.chapter, v.verse, v.text from verses as v inner join books as b on v.book = b.id where v.id < ? order by v.id desc limit 1'
+        const query = 'select v.id, b.id as bookId, b.name as book, v.chapter, v.verse, v.text from verses as v inner join books as b on v.book = b.id where v.id < ? order by v.id desc limit 1'
 
         connection.query(query, id, (error, results) => {
           if (error || results[0] === undefined) {
             errorHandler(error, `Não existe passagem anterior ao ID informado de: ${id}`, reject)
-            console.log(passages)
             return false
           }
           resolve({ passage: results[0] })
