9dabded5614d3282d1135ab84748db17b146d65b
Rafael Ribeiro <rafael.pr10@gmail.com>
2025-04-10 20:16:57 +0200

    Remove config for e2e

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,4 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  test: {
-    environmentMatchGlobs: [['src/controllers/**', 'prisma']],
-  },
 })
